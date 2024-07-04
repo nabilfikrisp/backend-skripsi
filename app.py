47daa2af3bd98326fc3ff280ccc313ef550f1ca9
nabilfikrisp <mnabilfikrisp@gmail.com>
2024-07-04 18:52:48 +0700

    fix: fix indentation at app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # app.py
-    from flask import Flask
-    from api.routes.api_routes import api_router
-    from flask_cors import CORS
+from flask import Flask
+from api.routes.api_routes import api_router
+from flask_cors import CORS
 
-    app = Flask(__name__)
-    cors = CORS(app)
-    app.register_blueprint(api_router)
+app = Flask(__name__)
+cors = CORS(app)
+app.register_blueprint(api_router)
 
 @app.route("/")
 def hello_world():
